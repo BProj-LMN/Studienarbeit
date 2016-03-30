c2c3e1fd59d2c6db992d280f375c8ef5a06e2297
Jannik Beyerstedt <code@jannikbeyerstedt.de>
2016-03-30 11:15:41 +0200

    gitignore eclipse stuff

--- Prog-Trackingsystem/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_videoio -lopencv_calib3d -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_features2d -lopencv_video -lopencv_objdetect  -lopencv_imgcodecs
-
