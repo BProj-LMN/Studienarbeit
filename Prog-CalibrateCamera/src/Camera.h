18dce6091047823b5b114d8d638976d539328ab0
Jannik Beyerstedt <code@jannikbeyerstedt.de>
2016-05-09 09:13:31 +0200

    [TIDY] fix typos + rename main files

--- Prog-CalibrateCamera/src/Camera.h
+++ Prog-CalibrateCamera/src/Camera.h
@@ -67,7 +67,7 @@ public:
   int get_rawFrame(Mat& frame);           // get original 3 channel frame, without mask
 
   // vector from Camera to object in world coordinates,
-  // be shure to reload settings after changing intrinsic parameters
+  // be sure to reload settings after changing intrinsic parameters
   int calcNewObjectRayVector(Point2f pixelPosition);
 
 private:
