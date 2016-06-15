6715b955ed25381cf349c619c6581e7553ddcae2
Jannik Beyerstedt <code@jannikbeyerstedt.de>
2016-06-15 13:06:37 +0200

    [CODE] Camera class changed to new software model

--- Prog-Trackingsystem/src/Camera.cpp
+++ Prog-Trackingsystem/src/Camera.cpp
@@ -21,60 +21,11 @@
 #include <iomanip>
 
 Camera::Camera(std::string configFile) {
-  std::cout << "Camera ctor stub\n";
+  globalMaskSet = 0;
+  parseConfig(configFile);
 }
 
-Camera::Camera(int cameraIndex) {
-  this->cameraID = cameraIndex;
-  intrinsicParamsLoaded = 0;
-  frameMaskSet = 0;
-
-#ifndef TEST
-  capture = cv::VideoCapture(cameraIndex);
-#else
-  if (0 == cameraIndex) {
-    capture = cv::VideoCapture("test/frameCam1-01.png");
-  } else if (1 == cameraIndex) {
-    capture = cv::VideoCapture("test/frameCam2-01.png");
-  }
-#endif
-  if (!capture.isOpened()) {
-    //return -1;
-  }
-
-  // use default filename
-  std::stringstream string;
-  string << DEFAULT_FILENAME << cameraID;
-  settingsFilename = string.str();
-}
-
-Camera::Camera(int cameraIndex, std::string settingsFile) {
-  this->cameraID = cameraIndex;
-  intrinsicParamsLoaded = 0;
-  frameMaskSet = 0;
-
-  capture = cv::VideoCapture(cameraIndex);
-  if (!capture.isOpened()) {
-    //return -1;
-  }
-
-  this->settingsFilename = settingsFile;
-}
-
-Camera::~Camera() {
-  capture.release();
-  std::cout << "[INFO]Camera::~Camera - released capture" << std::endl;
-}
-
-cv::VideoCapture Camera::get_capture() {
-  return capture;
-}
-
-int Camera::get_cameraID() {
-  return cameraID;
-}
-
-int Camera::correctDistortion(cv::Point2i src, cv::Point2f dst) {
+int Camera::undistort(PxPos src, PxPos dst) {
   // TODO Task: call undistortPoints()
   //undistortPoints(src, dst, *cameraMatrix, *distCoeffs);
 
@@ -83,169 +34,45 @@ int Camera::correctDistortion(cv::Point2i src, cv::Point2f dst) {
   return ERR;
 }
 
-int Camera::readSettings() {
-  return readSettings(settingsFilename);
-}
-
-int Camera::readSettings(std::string settingsFile) {
-  cv::FileStorage fs(settingsFile, cv::FileStorage::READ); // Read the settings
+int Camera::parseConfig(std::string configFile) {
+  cv::FileStorage fs(configFile, cv::FileStorage::READ); // Read the settings
   if (!fs.isOpened()) {
     fprintf(stderr, "ERROR: Camera::readSettings - opening file \n");
     return ERR;
   }
 
-  fs["frameMaskRect"] >> frameMaskRect;
   fs["cameraMatrix"] >> cameraMatrix;
   fs["distCoeffs"] >> distCoeffs;
-  fs["rvecs"] >> rvecs;
-  fs["tvecs"] >> tvecs;
-  fs["tvecs"] >> tvecs;
+  fs["globalMask"] >> globalMask_Rect;
   fs["positionVector"] >> positionVector;
-  fs["viewingCenter"] >> viewingCenter;
-  fs["viewingRight"] >> viewingRight;
-
+  fs["rotationMatrix"] >> rotationMatrix;
   fs.release();                                    // close Settings file
-  intrinsicParamsLoaded = 1;
-  if (frameMaskRect.area() > 0) {
-    set_frameMask(frameMaskRect);
-    frameMaskSet = 1;
-  }
-
-  setupRotationMatrix();
 
   return OK;
 }
 
-int Camera::saveSettings() {
-  return saveSettings(settingsFilename);
-}
-
-int Camera::saveSettings(std::string settingsFile) {
-  cv::FileStorage fs(settingsFile, cv::FileStorage::WRITE); // Read the settings
-  if (!fs.isOpened()) {
-    fprintf(stderr, "ERROR: Camera::saveSettings - opening file \n");
-    return ERR;
-  }
-
-  time_t tm;
-  time(&tm);
-  struct tm *t2 = localtime(&tm);
-  fs << "timestamp" << (int) tm;
-  char buf[1024];
-  strftime(buf, sizeof(buf) - 1, "%c", t2);
-  fs << "datetime" << buf;
-
-  fs << "cameraMatrix" << cameraMatrix;
-  fs << "distCoeffs" << distCoeffs;
-  fs << "globalMask" << frameMaskRect;
-  fs << "positionVector" << positionVector;
-  fs << "rotationMatrix" << rotationMatrix;
-  fs << "viewingCenter" << viewingCenter;
-  fs << "viewingRight" << viewingRight;
-
-  fs.release();                                    // close Settings file
-  return OK;
-}
-
-int Camera::set_frameMask(cv::Rect frameMask) {
-  if (frameMask.area() > 0) {
-    cv::Mat frame;
-    capture >> frame;
-    this->frameMask = cv::Mat::zeros(frame.rows, frame.cols, CV_8U);
-    this->frameMask(frameMask) = 255;
-
-    this->frameMaskRect = frameMask;
-
-    frameMaskSet = 1;
+void Camera::initGlobalMask(cv::Mat& frame) {
+  if (globalMask_Rect.area() > 0) {
+    this->globalMask = cv::Mat::zeros(frame.rows, frame.cols, CV_8U);
+    this->globalMask(globalMask_Rect) = 255;
+    globalMaskSet = 1;
   } else {
-    std::cout << "[ERROR] Camera::set_frameMask - no valid frameMask given in" << std::endl;
+    std::cout << "[ERROR] Camera::initGlobalMask - area of globalMask_Rect is 0" << std::endl;
   }
-
-  return OK;
 }
 
-int Camera::get_newFrame(cv::Mat& frame) {
-#ifdef TEST
-  if (capture.get(cv::CAP_PROP_POS_FRAMES) >= capture.get(cv::CAP_PROP_FRAME_COUNT)) {
-    capture.set(cv::CAP_PROP_POS_FRAMES, 0);
+void Camera::addGlobalMaskToFrame(cv::Mat& frame) {
+  if (globalMaskSet) {
+    frame = frame & globalMask;
   }
-#endif
-
-  capture >> frame;
-
-  cvtColor(frame, frame, CV_BGR2GRAY);
-
-  //vector<Mat> channels;
-  //split(frame, channels);
-  //frame = channels[0];
-
-  if (frameMaskSet) {
-    frame = frame & frameMask;
-  }
-
-  return OK;
 }
 
-int Camera::get_rawFrame(cv::Mat& frame) {
-#ifdef TEST
-  if (capture.get(cv::CAP_PROP_POS_FRAMES) >= capture.get(cv::CAP_PROP_FRAME_COUNT)) {
-    capture.set(cv::CAP_PROP_POS_FRAMES, 0);
-  }
-#endif
-
-  capture >> frame;
-
-  return OK;
-}
-
-int Camera::setupRotationMatrix() {
-  /*
-   * calculates euler rotation angles
-   * and calculate rotation matrix for all rotations at once
-   */
-
-  // some variables
-  cv::Mat Xaxis = (cv::Mat_<float>(3, 1) << 1, 0, 0);
-  cv::Mat Yaxis = (cv::Mat_<float>(3, 1) << 0, 1, 0);
-  cv::Mat Euler1, Euler2, Euler3;
-
-  cv::Point3i r = viewingCenter - positionVector;
-  cv::Point3i s = viewingRight - positionVector;
-  cv::Mat r_vec = (cv::Mat_<float>(3, 1) << r.x, r.y, r.z);
-  cv::Mat s_vec = (cv::Mat_<float>(3, 1) << s.x, s.y, s.z);
-
-  // calculate euler angles
-  float w1 = -atan2(r.y, r.x);
-  float w2 = -asin(r.z / sqrt(r.x * r.x + r.y * r.y + r.z * r.z));
-
-  euler1(w1, Euler1);
-  euler2(w2, Euler2);
-
-  cv::Mat Xaxis_new = Euler2 * Euler1 * Xaxis;
-  cv::Mat Yaxis_new = Euler2 * Euler1 * Yaxis;
-  cv::Mat plane_reference = Xaxis_new.cross(Yaxis_new);
-  cv::Mat plane_camera = s_vec.cross(r_vec);
-  float w3 = -acos(plane_reference.dot(plane_camera) / (norm(plane_reference) * norm(plane_camera)));
-  euler3(w3, Euler3);
-
-  std::cout << "Camera::setupRotationMatrix of camera " << cameraID << "\n";
-  std::cout << "w1 " << std::fixed << std::setprecision(5) << std::setw(8) << w1 << "\n";
-  std::cout << "w2 " << std::fixed << std::setprecision(5) << std::setw(8) << w2 << "\n";
-  std::cout << "w3 " << std::fixed << std::setprecision(5) << std::setw(8) << w3 << "\n";
-  std::cout << "\n" << std::flush;
-
-  // calculate euler rotation matrix
-  this->rotationMatrix = Euler3 * Euler2 * Euler1;
-
-  return OK;
-}
-
-int Camera::calcNewObjectRayVector(cv::Point2f pixelPosition) {
-  /*
-   * calculates objectRay for use in triangulation out of a sensor pixelPosition
-   *
-   * pixelPosition is float instead of int, because there are subpixel values after undistort
-   */
+/*
+ * calculates objectRay for use in triangulation from a sensor pixelPosition
+ */
+int Camera::calcObjRay(PxPos pixelPos, VectRay objectRay) {
+  cv::Point2f pixelPosition{pixelPos.x, pixelPos.y};
+  Pos3D direction;
 
   // calculate object ray in world coordinates from pixel position
   cv::Point3f ray;
@@ -257,20 +84,21 @@ int Camera::calcNewObjectRayVector(cv::Point2f pixelPosition) {
   // transform vector to world coordinates by multiplying rotationMatrix in front of the vector
   cv::Mat objectRay_Mat = rotationMatrix * objectRayCameraCoord;
 
-  this->objectVector.x = objectRay_Mat.at<float>(0, 0);
-  this->objectVector.y = objectRay_Mat.at<float>(1, 0);
-  this->objectVector.z = objectRay_Mat.at<float>(2, 0);
+  direction.x = objectRay_Mat.at<float>(0, 0);
+  direction.y = objectRay_Mat.at<float>(1, 0);
+  direction.z = objectRay_Mat.at<float>(2, 0);
 
+  objectRay.dir = direction;
+  objectRay.pos = positionVector;
   return OK;
 }
 
+/*
+ * calculates object ray from pixel value on the sensor. Vector is in camera coordinate system
+ *
+ * pixelPosition.x <=> u , pixelPosition.y <=> v
+ */
 int Camera::calcObjectRayInCameraCoordinates(cv::Point2f pixelPosition, cv::Point3f& objectRay) {
-  /*
-   * calculates object ray from pixel value on the sensor. Vector is in camera coordinate system
-   *
-   * pixelPosition.x <=> u , pixelPosition.y <=> v
-   */
-
   float c_x = cameraMatrix.at<double>(0, 2);
   float c_y = cameraMatrix.at<double>(1, 2);
   float f = cameraMatrix.at<double>(0, 0);
@@ -280,60 +108,5 @@ int Camera::calcObjectRayInCameraCoordinates(cv::Point2f pixelPosition, cv::Poin
   objectRay.x = f;                     // w --> X
 
   return OK;
-}
-
-/*
- * euler rotations for the following scheme
- * rotate Z, then Y', then X''
- *
- * correspondance Camera Coordinate System to World Coordinate System:
- * u,v,w --> X,Y,Z or more precisely X'',Y'',Z''
- * X --> w, Y --> u, Z --> v
- * ==> X-Axis is camera viewing axis !
- * ==> with this correspondance, camera is already upside down
- */
-void Camera::euler1(float angle, cv::Mat& matrix) {
-  /* first euler rotation
-   * Matrix = [ [ cos(alpha)  sin(alpha)  0];
-   [-sin(alpha)  cos(alpha)  0];
-   [ 0           0           1] ];
-   */
-  double m[3][3] = { { cos(angle), sin(angle), 0 }, { -sin(angle), cos(angle), 0 }, { 0, 0, 1 } };
-  cv::Mat result = cv::Mat(3, 3, CV_32F, m);
-  result.copyTo(matrix);
-}
-void Camera::euler2(float angle, cv::Mat& matrix) {
-  /* second euler rotation
-   * Matrix = [ [ cos(alpha)  0          -sin(alpha)];
-   [ 0           1           0];
-   [ sin(alpha)  0           cos(alpha)] ];
-   */
-  double m[3][3] = { { cos(angle), 0, -sin(angle) }, { 0, 1, 0 }, { sin(angle), 0, cos(angle) } };
-  cv::Mat result = cv::Mat(3, 3, CV_32F, m);
-  result.copyTo(matrix);
-}
-void Camera::euler3(float angle, cv::Mat& matrix) {
-  /* third euler rotation
-   * Matrix = [ [ 1           0           0];
-   [ 0           cos(alpha)  sin(alpha)];
-   [ 0          -sin(alpha)  cos(alpha)] ];
-   */
-  double m[3][3] = { { 1, 0, 0 }, { 0, cos(angle), sin(angle) }, { 0, -sin(angle), cos(angle) } };
-  cv::Mat result = cv::Mat(3, 3, CV_32F, m);
-  result.copyTo(matrix);
-}
-float Camera::norm(cv::Mat column_vector) {
-  float result = 0;
-
-  if (column_vector.rows > 1 && column_vector.cols == 1) {
-    for (int i = 0; i < column_vector.rows; i++) {
-      result += column_vector.at<float>(i, 0) * column_vector.at<float>(i, 0);
-    }
-    result = sqrt(result);
-  } else {
-    fprintf(stderr, "ERROR: in Camera::vecNorm - no column vector given \n");
-  }
-
-  return result;
 }
 
--- Prog-Trackingsystem/src/Camera.h
+++ Prog-Trackingsystem/src/Camera.h
@@ -10,67 +10,36 @@
 #ifndef SRC_CAMERA_H_
 #define SRC_CAMERA_H_
 
-#define TEST  // for testing the system without cameras
-
-#define DEFAULT_FILENAME "cameraStorage-"
+#include "DataFormats.h"
+//#include "myGlobalConstants.h"
+#include "Logger.h"
 
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 
 class Camera {
-  cv::VideoCapture capture;
-  int cameraID;
-  std::string settingsFilename;
-  cv::Rect frameMaskRect;
-  cv::Mat frameMask;
-  bool frameMaskSet;
-
-  cv::Point3i viewingCenter;  // central image point (p) - will be saved and loaded
-  cv::Point3i viewingRight;   // second image point  (t) - will be saved and loaded
-  cv::Point3f cameraRotation; // 3 euler rotations   (w) - will be computed
-  cv::Mat rotationMatrix;     // 3x3 float !!!
-
 public:
-  cv::Point3i positionVector; // position            (o) - will be saved and loaded
-  cv::Point3i objectVector;   // from calcNewObjectRayVector
+  Camera(std::string configFile);
+
+  int undistort(PxPos src, PxPos dst);
+  int calcObjRay(PxPos pixelPos, VectRay objectRay);
+
+  void initGlobalMask(cv::Mat& frame);
+  void addGlobalMaskToFrame(cv::Mat& frame);
+  bool globalMaskSet;
+
+private:
+  int parseConfig(std::string configFile);
+
+  int calcObjectRayInCameraCoordinates(cv::Point2f pixelPosition, cv::Point3f& objectRayCameraCoord);
 
   cv::Mat cameraMatrix;
   cv::Mat distCoeffs;
-  cv::Mat rvecs;
-  cv::Mat tvecs;
-  bool intrinsicParamsLoaded;
+  cv::Rect globalMask_Rect;
+  cv::Point3i positionVector;
+  cv::Mat rotationMatrix;
 
-public:
-  Camera(int cameraIndex);
-  Camera(int cameraIndex, std::string settingsFile);
-  Camera(std::string configFile);
-
-  virtual ~Camera();
-
-  cv::VideoCapture get_capture();
-  int correctDistortion(cv::Point2i src, cv::Point2f dst);
-
-  int readSettings(std::string settingsFile); // read from temporarily filename
-  int readSettings();                         // read from filename from constructor
-  int saveSettings(std::string settingsFile); // save to temporarily filename
-  int saveSettings();                         // save to filename from constructor
-  int get_cameraID();
-  int set_frameMask(cv::Rect frameMask);
-  int get_newFrame(cv::Mat& frame);
-  int get_rawFrame(cv::Mat& frame);           // get original 3 channel frame, without mask
-
-  // vector from Camera to object in world coordinates,
-  // be sure to reload settings after changing intrinsic parameters
-  int calcNewObjectRayVector(cv::Point2f pixelPosition);
-
-private:
-  int setupRotationMatrix(); // call after changing camera position information - or reload setting
-  int calcObjectRayInCameraCoordinates(cv::Point2f pixelPosition, cv::Point3f& objectRayCameraCoord);
-
-  void euler1(float angle, cv::Mat& matrix);
-  void euler2(float angle, cv::Mat& matrix);
-  void euler3(float angle, cv::Mat& matrix);
-  float norm(cv::Mat column_vector);
+  cv::Mat globalMask;
 };
 
 #endif /* SRC_CAMERA_H_ */
